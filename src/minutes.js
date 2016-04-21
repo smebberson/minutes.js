e9eb3c1f53b2cadae758708076f0a933b7d672d5
Scott Mebberson <scott@scottmebberson.com>
2016-04-21 14:15:21 +0000

    Added comments, tokenized the time units, made time units verbose by default and improved toString() time unit concatenation.

--- src/minutes.js
+++ src/minutes.js
@@ -1,92 +1,115 @@
 'use strict';
 
 class Minutes {
-    
+
     constructor(minutes, opts) {
-        
+
+        // Atempt to parse the minutes as an integer.
         this.minutes = parseInt(minutes);
-        
+
+        // Only accept an integer.
         if (Number.isNaN()) {
             throw new Error('Minutes accepts an integer representing minutes.');
         }
-        
+
+        // Accept some options, default those that don't exist.
         this.opts = opts || {};
-        
+
+        // Default the time units.
+        this.opts.units = this.opts.units || {};
+        this.opts.units.w = this.opts.units.w || 'week';
+        this.opts.units.d = this.opts.units.d || 'day';
+        this.opts.units.h = this.opts.units.h || 'hour';
+        this.opts.units.m = this.opts.units.m || 'minute';
+
     }
-    
+
     toString () {
-        
+
+        // The delta will reduce in scope as we move through the various time units.
+        // Each time a time unit is matched, remove that unit from the delta.
         var delta = this.minutes,
             parts = [];
-        
+
         // Determine the time period.
+
+        // Are the minutes greater than a week?
         if (delta >= Minutes.week()) {
-            parts.push(Minutes.formatPart(Math.floor(delta/Minutes.week()), 'w'));
+            parts.push(this.formatPart(Math.floor(delta/Minutes.week()), 'w'));
             delta -= Math.floor(delta/Minutes.week())*Minutes.week();
         }
-        
+
+        // Are the remaining(?) minutes greater than a day?
         if (delta >= Minutes.day()) {
-            parts.push(Minutes.formatPart(Math.floor(delta/Minutes.day()), 'd'));
+            parts.push(this.formatPart(Math.floor(delta/Minutes.day()), 'd'));
             delta -= Math.floor(delta/Minutes.day())*Minutes.day();
         }
-        
+
+        // Are the remaining(?) minutes greather than an hour?
         if (delta >= Minutes.hour()) {
-            parts.push(Minutes.formatPart(Math.floor(delta/Minutes.hour()), 'h'));
+            parts.push(this.formatPart(Math.floor(delta/Minutes.hour()), 'h'));
             delta -= Math.floor(delta/Minutes.hour())*Minutes.hour();
         }
-        
+
+        // Are there any remaining minutes?
         if (delta > 0 && delta < Minutes.hour()) {
-            parts.push(Minutes.formatPart(delta, 'm'));
+            parts.push(this.formatPart(delta, 'm'));
         }
-        
-        return parts.join(' and ');
-        
+
+        // Join parts with `,`, other than the final one which should be `and`.
+        return parts.join(', ').replace(/,(?!.*,)/, ' and');
+
     }
-    
-    static formatPart (value, part) {
-        
-        var str = String(value);
-        
-        switch (part) {
-            
-            case 'm':
-                str += ' min';
-                break;
-            
-            case 'h':
-                str += ' hr';
-                break;
-            
-            case 'd':
-                str += ' day';
-                break;
-            
-            case 'w':
-                str += ' wk';
-                break;
-            
-        }
-        
+
+    //
+    // Instance methods.
+    //
+
+    /*
+     * Format a time unit (i.e. 1 hour as '1 hour').
+     * @return {String}
+    */
+    formatPart (value, unit) {
+
+        var str = `${value} ${this.opts.units[unit]}`;
+
+        // Make the unit representation plural if required.
         if (value > 1) {
             str += 's';
         }
-        
+
         return str;
-        
+
     }
-    
+
+    //
+    // Static methods.
+    //
+
+    /*
+     * Return the number of minutes in an hour.
+     * @return {Number}
+    */
     static hour () {
         return 60;
     }
-    
+
+    /*
+     * Return the number of minutes in a day.
+     * @return {Number}
+    */
     static day () {
         return 24*Minutes.hour();
     }
-    
+
+    /*
+     * Return the number of minutes in a week.
+     * @return {Number}
+    */
     static week () {
         return 7*Minutes.day();
     }
-    
+
 }
 
 module.exports = Minutes;
--- test/minutes.js
+++ test/minutes.js
@@ -4,21 +4,21 @@ let expect = require('chai').expect,
     Minutes = require('../src/minutes');
 
 describe('Minutes will', () => {
-    
-    it('will throw if not passed an integer', function () {
-        
+
+    it('throw if not passed an integer', function () {
+
         var fn = function () {
             new Minutes('23.3');
         }
-        
+
         expect(fn).to.throw;
-        
+
     });
-    
+
     describe('format', function () {
-        
+
         describe('single time units', function () {
-            
+
             var minute = new Minutes(1),
                 minutes = new Minutes(10),
                 hour = new Minutes(1*60),
@@ -27,85 +27,85 @@ describe('Minutes will', () => {
                 days = new Minutes(2*24*60),
                 week = new Minutes(7*24*60),
                 weeks = new Minutes(2*7*24*60);
-        
+
             it('a minute', function () {
-                
-                expect(minute.toString()).to.equal('1 min');
-                
+
+                expect(minute.toString()).to.equal('1 minute');
+
             });
-            
+
             it('minutes', function () {
-                
-                expect(minutes.toString()).to.equal('10 mins');
-                
+
+                expect(minutes.toString()).to.equal('10 minutes');
+
             });
-            
+
             it('an hour', function () {
-                
-                expect(hour.toString()).to.equal('1 hr');
-                
+
+                expect(hour.toString()).to.equal('1 hour');
+
             });
-            
+
             it('hours', function () {
-                
-                expect(hours.toString()).to.equal('2 hrs');
-                
+
+                expect(hours.toString()).to.equal('2 hours');
+
             });
-            
+
             it('a day', function () {
-                
+
                 expect(day.toString()).to.equal('1 day');
-                
+
             });
-            
+
             it('days', function () {
-                
+
                 expect(days.toString()).to.equal('2 days');
-                
+
             });
-            
+
             it('a week', function () {
-                
-                expect(week.toString()).to.equal('1 wk');
-                
+
+                expect(week.toString()).to.equal('1 week');
+
             });
-            
+
             it('weeks', function () {
-                
-                expect(weeks.toString()).to.equal('2 wks');
-                
+
+                expect(weeks.toString()).to.equal('2 weeks');
+
             });
-            
+
         });
-        
+
         describe('multiple time units', function () {
-            
+
            it('hours and minutes', function () {
-            
+
                 var hours = new Minutes(2*Minutes.hour()+10);
-                
-                expect(hours.toString()).to.equal('2 hrs and 10 mins');
-                
+
+                expect(hours.toString()).to.equal('2 hours and 10 minutes');
+
             });
-            
+
             it('days, hours and minutes', function () {
-            
+
                 var hours = new Minutes((3*24*60)+(2*60)+10);
-                
-                expect(hours.toString()).to.equal('3 days and 2 hrs and 10 mins');
-                
+
+                expect(hours.toString()).to.equal('3 days, 2 hours and 10 minutes');
+
             });
-            
+
             it('weeks, days, hours and minutes', function () {
-            
+
                 var hours = new Minutes((2*7*24*60)+(3*24*60)+(2*60)+10);
-                
-                expect(hours.toString()).to.equal('2 wks and 3 days and 2 hrs and 10 mins');
-                
+
+                expect(hours.toString()).to.equal('2 weeks, 3 days, 2 hours and 10 minutes');
+
             });
-            
+
         });
-        
+
     });
-    
+
 });
