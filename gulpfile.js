2c59d95b95c7f4fc6f4af0e51c1e49c65ba276dd
Scott Mebberson <scott@scottmebberson.com>
2016-04-23 04:57:34 +0000

    Added travis-ci.com support and codecov.io.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp'),
     babel = require('gulp-babel');
 
 gulp.task('default', function () {
-    
+
     return gulp.src('src/minutes.js')
         .pipe(babel())
         .pipe(gulp.dest('dist'));
-    
+
 });
